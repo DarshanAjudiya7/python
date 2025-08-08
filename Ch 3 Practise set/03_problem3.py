7da61a59b3a5c3104330dc8d7f0d42703d766290
Darshan Ajudiya <darshanajudiya07@gmail.com>
2025-08-08 21:27:04 +0530

    Delete Ch 3 Practise set directory
    delete ch 3

--- Ch 3 Practise set/03_problem3.py
+++
@@ -1,4 +0,0 @@
-# Write a program to detect double space in a string.
-
-str1 = "my name  is Darshan  Ajudiya. I  am very  good in  nature"
-print(str1.find("  "))
--- Ch 3 Practise set/04_problem4.py
+++
@@ -1,4 +0,0 @@
-# Replace the double space from problem 3 with single spaces.
-
-str1 = "my name  is Darshan  Ajudiya. I  am very  good in  nature."
-print(str1.replace("  "," "))
