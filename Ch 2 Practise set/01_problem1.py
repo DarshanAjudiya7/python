fa5f48d54cb48d8a5048648b334fdaea0d4d65ad
Darshan Ajudiya <darshanajudiya07@gmail.com>
2025-08-08 21:26:37 +0530

    Delete Ch 2 Practise set directory
    delete ch 2

--- Ch 2 Practise set/01_problem1.py
+++
@@ -1,8 +0,0 @@
-# Write a python program to add two numbers.
-
-a = int(input("enter 1st number : "))
-b = int(input("enter 2nd number : "))
-
-sum = a + b
-
-print(f"sum of two numbers are", + sum)
--- Ch 2 Practise set/03_problem3.py
+++
@@ -1,4 +0,0 @@
-# Check the type of variable assigned using input () function,
-
-a = input("enter the value of a :")
-print(type(a))
