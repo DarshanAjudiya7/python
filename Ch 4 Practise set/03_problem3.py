a62bff4f0ae181f0bfd8609608562664fb595c6b
Darshan Ajudiya <darshanajudiya07@gmail.com>
2025-08-08 21:27:35 +0530

    Delete Ch 4 Practise set directory
    delete ch 4

--- Ch 4 Practise set/03_problem3.py
+++
@@ -1,5 +0,0 @@
-# Check that a tuple type cannot be changed in python.
-
-a = ( 32 , 54 , "Darshan")
-
-a[2] = "Samir"  # becaise tupples are immutable
--- Ch 4 Practise set/04_problem4.py
+++
@@ -1,5 +0,0 @@
-# Write a program to sum a list with 4 numbers.
-
-a = [23 , 43 , 76 , 78]
-
-print(sum(a))
